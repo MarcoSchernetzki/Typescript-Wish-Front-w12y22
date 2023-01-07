2a23a6a79d8890cef1e2e1e28953b6edd6f090e1
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-08 00:45:26 +0100

    Add update form

---
+++ src/feature/components/update.form/update.form.tsx
@@ -0,0 +1,110 @@
+import { SyntheticEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUsers } from '../../users/hook/use.users';
+import { useWishes } from '../../wishes/hook/use.wishes';
+
+export function UpdateForm() {
+    const navigate = useNavigate();
+
+    const { users } = useUsers();
+    const { wishes, handleUpdate } = useWishes();
+    const [updateFormState, setUpdateFormState] = useState({
+        name: wishes.selectedWish?.name,
+        image: wishes.selectedWish?.image,
+        origin: wishes.selectedWish?.origin,
+        price: wishes.selectedWish?.price,
+        comments: wishes.selectedWish?.comments,
+    });
+
+    const handleInput = (ev: SyntheticEvent) => {
+        const element = ev.target as HTMLFormElement;
+        setUpdateFormState({
+            ...updateFormState,
+            [element.name]: element.value,
+        });
+    };
+
+    const handleUpdateSubmit = (ev: SyntheticEvent) => {
+        ev.preventDefault();
+        handleUpdate(
+            wishes.selectedWish?.id as string,
+            updateFormState,
+            users.token as string
+        );
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleUpdateSubmit}>
+                <label>
+                    <input
+                        placeholder="name"
+                        type="text"
+                        name="name"
+                        value={updateFormState.name}
+                        onInput={handleInput}
+                    />
+                </label>
+                <label>
+                    <input
+                        type="url"
+                        name="image"
+                        value={updateFormState.image}
+                        onInput={handleInput}
+                    />
+                </label>
+                <label>
+                    <input
+                        type="text"
+                        name="origin"
+                        value={updateFormState.origin}
+                        onInput={handleInput}
+                    />
+                </label>
+                <label>
+                    <input
+                        type="text"
+                        name="price"
+                        value={updateFormState.price}
+                        onInput={handleInput}
+                    />
+                </label>
+                <label>
+                    <input
+                        type="text"
+                        name="comments"
+                        value={updateFormState.comments}
+                        onInput={handleInput}
+                    />
+                </label>
+                <label>
+                    <button type="submit">Guardar</button>
+                    <button
+                        onClick={() => {
+                            navigate('/home');
+                        }}
+                    >
+                        Cancelar
+                    </button>
+                </label>
+            </form>
+        </div>
+    );
+}
+
+export default UpdateForm;
+
+/*
+                        onClick={(e) => {
+                            e.preventDefault();
+                            handleUpdate(
+                                wishes.selectedWish?.id as string,
+                                updateFormState as WishI,
+                                users.token as string
+                            );
+                            // handleGet(users.user?.id as string);
+                            if (users.isLogged) {
+                                navigate('/home');
+                            }
+                        }}
+                        */
