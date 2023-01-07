b0a73d3f2f1c0b22416dad3b69864277dd87669f
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-07 14:27:10 +0100

    Add route home page

--- src/infrastructure/components/routes/routes.tsx
+++ src/infrastructure/components/routes/routes.tsx
@@ -1,9 +1,12 @@
-import { Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from '../../../feature/pages/homePage/home.page';
 
 export function AppRoutes() {
     return (
         <Routes>
-            {/* <Route path="*" element={<Navigate replace to="" />}></Route> */}
+            <Route path="home" element={<HomePage />}></Route>
+
+            <Route path="*" element={<Navigate replace to="home" />}></Route>
         </Routes>
     );
 }
