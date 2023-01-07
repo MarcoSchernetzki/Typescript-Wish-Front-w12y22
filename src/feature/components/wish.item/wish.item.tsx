66e68c6369ffbc6f3257792784f099fc007d6ed7
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-07 18:45:58 +0100

    Add wish item

---
+++ src/feature/components/wish.item/wish.item.tsx
@@ -0,0 +1,23 @@
+import { WishI } from '../../wishes/model/wish';
+import { useWishes } from '../../wishes/hook/use.wishes';
+
+export function WishItem({ item }: { item: WishI }) {
+    const { handleSelect } = useWishes();
+
+    return (
+        <>
+            <div className="container">
+                <img
+                    className="image"
+                    src={item.image}
+                    alt={item.name}
+                    width="150px"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleSelect(item);
+                    }}
+                />
+            </div>
+        </>
+    );
+}
