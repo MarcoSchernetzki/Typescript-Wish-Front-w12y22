1d3359fc0f4364bd52abebcc0cf6258be40bb682
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-07 20:58:50 +0100

    Add test create form

---
+++ src/feature/components/create.form/create.form.test.tsx
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { appStore } from '../../../infrastructure/store/store';
+import CreateForm from './create.form';
+
+describe('Given the create.form component', () => {
+    beforeEach(() => {
+        render(
+            <Router>
+                <Provider store={appStore}>
+                    <CreateForm />
+                </Provider>
+            </Router>
+        );
+    });
+
+    test('It should display «cancelar» button', () => {
+        const firstTextInput = screen.getByPlaceholderText('Nombre');
+        expect(firstTextInput).toBeInTheDocument();
+        userEvent.click(screen.getByText(/cancelar/i));
+    });
+
+    test('It should display «guardar» button', () => {
+        const element = screen.getByText(/guardar/i);
+        expect(element).toBeInTheDocument();
+        fireEvent.click(screen.getByText('Guardar'));
+        fireEvent.input(screen.getByPlaceholderText('Nombre'));
+    });
+});
