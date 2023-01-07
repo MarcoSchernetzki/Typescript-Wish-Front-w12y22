14635d80b1c618236ca583572d47ea760fae4329
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-07 12:51:50 +0100

    Add file store

---
+++ src/infrastructure/store/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+export const appStore = configureStore({
+    reducer: {},
+});
+
+export type rootStore = typeof appStore;
+export type rootState = ReturnType<typeof appStore.getState>;
