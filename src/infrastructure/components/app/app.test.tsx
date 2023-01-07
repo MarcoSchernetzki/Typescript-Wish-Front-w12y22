bcf9416b45341515bf9c662d3fad1605d0354bc4
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-07 12:49:54 +0100

    Fixing test app

--- src/infrastructure/components/app/app.test.tsx
+++ src/infrastructure/components/app/app.test.tsx
@@ -1,14 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './app';
+import { Provider } from 'react-redux';
+import { appStore } from '../../store/store';
 
 describe('Given app component', () => {
     describe('When we render the component', () => {
         beforeEach(() => {
-            render(<App />);
+            render(
+                <Router>
+                    <Provider store={appStore}>
+                        <App />
+                    </Provider>{' '}
+                </Router>
+            );
         });
         test('Then it should display the title', () => {
-            const element = screen.getByText(/mundo/i);
+            const element = screen.getByText(/Acceso/i);
             expect(element).toBeInTheDocument();
         });
     });
