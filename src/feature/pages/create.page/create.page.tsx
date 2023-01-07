66f513996878835d73ec1b55a20e5211649544fb
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-07 21:00:07 +0100

    Add create page

---
+++ src/feature/pages/create.page/create.page.tsx
@@ -0,0 +1,15 @@
+import CreateForm from '../../components/create.form/create.form';
+
+function CreatePage() {
+    return (
+        <>
+            <main>
+                <h2>Añade un deseo</h2>
+                <div>
+                    <CreateForm />
+                </div>
+            </main>
+        </>
+    );
+}
+export default CreatePage;
