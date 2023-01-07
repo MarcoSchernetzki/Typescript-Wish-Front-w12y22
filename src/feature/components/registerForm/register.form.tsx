19eda0b794aee5b02becc73bbcb0911ab4f38604
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-07 17:57:44 +0100

    Add register form

---
+++ src/feature/components/registerForm/register.form.tsx
@@ -0,0 +1,65 @@
+import { SyntheticEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserRepository } from '../../users/service/user.repository';
+
+export function RegisterForm() {
+    const navigate = useNavigate();
+    const userRepo = new UserRepository();
+
+    const [registerFormState, setRegisterFormState] = useState({
+        name: '',
+        email: '',
+        passwd: '',
+    });
+
+    const handleSubmitRegister = (ev: SyntheticEvent) => {
+        ev.preventDefault();
+        userRepo.register(registerFormState);
+        navigate('/login');
+    };
+
+    const handleInput = (ev: SyntheticEvent) => {
+        const element = ev.target as HTMLFormElement;
+        setRegisterFormState({
+            ...registerFormState,
+            [element.name]: element.value,
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmitRegister}>
+            <label>
+                <input
+                    type="text"
+                    name="name"
+                    required
+                    placeholder="Nombre"
+                    onInput={handleInput}
+                />
+            </label>
+            <label>
+                <input
+                    type="text"
+                    name="email"
+                    required
+                    placeholder="Email"
+                    onInput={handleInput}
+                />
+            </label>
+            <label>
+                <input
+                    type="password"
+                    name="passwd"
+                    required
+                    placeholder="Contraseña"
+                    onInput={handleInput}
+                />
+            </label>
+            <label>
+                <button type="submit">Crear una cuenta</button>
+            </label>
+        </form>
+    );
+}
+
+export default RegisterForm;
