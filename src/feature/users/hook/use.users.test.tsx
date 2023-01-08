624d4362aa7a674ebc2ae22fce2e280c555f02f6
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-08 01:17:15 +0100

    Fix test useUser

--- src/feature/users/hook/use.users.test.tsx
+++ src/feature/users/hook/use.users.test.tsx
@@ -1,5 +1,6 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { appStore } from '../../../infrastructure/store/store';
 import { mockStore, userMock, userMockToken } from '../../../mock/mock.store';
 import { ProtoUserI, UserI } from '../model/user';
 import { UserRepository } from '../service/user.repository';
@@ -24,19 +25,37 @@ let result: {
 };
 
 describe('Given useUsers', () => {
-    beforeEach(() => {
-        UserRepository.prototype.login = jest
-            .fn()
-            .mockResolvedValue(userMockToken);
+    describe('When it has been run handleLogin and it has called handleLogin', () => {
+        test('Then should return a promise of user', () => {
+            UserRepository.prototype.login = jest
+                .fn()
+                .mockResolvedValue(userMockToken);
+            act(() => {
+                const wrapper = ({ children }: { children: JSX.Element }) => (
+                    <Provider store={mockStore}>{children}</Provider>
+                );
 
-        const wrapper = ({ children }: { children: JSX.Element }) => (
-            <Provider store={mockStore}>{children}</Provider>
-        );
-
-        ({ result } = renderHook(() => useUsers(), { wrapper }));
+                ({ result } = renderHook(() => useUsers(), { wrapper }));
+            });
+            result.current.handleLogin({
+                email: userMock.email,
+                passwd: userMock.passwd,
+            } as UserI);
+            expect(UserRepository.prototype.login).toHaveBeenCalled();
+        });
     });
     describe('When it has been run handleLogin and it has called handleLogin', () => {
         test('Then should return a promise of user', () => {
+            UserRepository.prototype.login = jest
+                .fn()
+                .mockResolvedValue(userMock);
+            act(() => {
+                const wrapper = ({ children }: { children: JSX.Element }) => (
+                    <Provider store={appStore}>{children}</Provider>
+                );
+
+                ({ result } = renderHook(() => useUsers(), { wrapper }));
+            });
             result.current.handleLogin({
                 email: userMock.email,
                 passwd: userMock.passwd,
